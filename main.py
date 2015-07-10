63a1cbc1cb0c2741bf8bdf49a2538ad3ec136c7c
Euphe <euphetar@gmail.com>
2015-07-11 00:05:11 +0300

    minor fix

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from bot import DungeonBot
-from mock_telegram import MockAPI
 import telegram
 
 # read token from file
 
-#tg = telegram.Bot(token='token')
-tg = MockAPI()
+tg = telegram.Bot(token='token')
+#tg = MockAPI()
 
 DungeonBot.api = tg
 
